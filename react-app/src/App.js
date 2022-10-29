8e7a456a188d5a9e600202a203427c29849d1841
serg473 <sergeidsi473@gmail.com>
2022-10-29 14:03:41 +0300

    Counter

---
+++ react-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react'
+
+function App() {
+    const [count, setCount] = React.useState(0);
+    const onclickPlus = () =>{
+        setCount(count + 1);
+    }
+    const onClickMinus = () =>{
+        setCount(count - 1);
+    }
+    return (
+        <div className="App">
+            <div>
+                <h2>Счетчик:</h2>
+                <h1>{count}</h1>
+                <div className='block_button'>
+                    <button onClick = {onClickMinus} className='minus'>- Minus</button>
+                    <button onClick={onclickPlus} className='plus'>+ Plus</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
