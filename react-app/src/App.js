758e12ad9657fbb7ddd7cd2dafe9b394ef00f58b
serg473 <sergeidsi473@gmail.com>
2022-10-29 19:29:16 +0300

    create modal-window

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react'
 
-function App() {
+
+const Modal = ({open, setOpen}) => {
+    return (
+        <div id="myModal" className={`modal animated ${open ? 'show' : ''}`}>
+            <div className="modal-content">
+                <span onClick={() => setOpen(false)} className="close">&times;</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque earum eos, fugit nihil
+                    possimus provident quisquam repellat ullam. Alias, amet, aspernatur aut corporis esse eum
+                    facilis laudantium libero nam non possimus quam quasi quia, quis sint. Ad animi autem dicta
+                    ex optio perspiciatis qui, quibusdam quis recusandae reiciendis sit temporibus.</p>
+                <img src="https://media1.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.gif" alt=""/>
+            </div>
+
+        </div>
+    );
+};
+
+const App = () => {
     const [open, setOpen] = React.useState(false);
     return (
         <div className='App'>
             <button onClick={() => setOpen(true)} className='open_modal_btn'>Open modal-window</button>
-            {
-                open && (
-                    <div className='overlay'>
-                        <div className="modal">
-                            <div onClick={() => setOpen(false)} className='close'><img
-                                src="https://img.icons8.com/ios/344/cancel.png" alt=""/></div>
-                            <div><img
-                                src="https://media2.giphy.com/media/hUL5gdlvDgtRbOElZS/giphy.gif?cid=6c09b952ouig7kj7que9462ddztke6n5gyavk3bnruvxu2nx&rid=giphy.gif&ct=s"
-                                alt=""/></div>
-                        </div>
-                    </div>
-                )
-            }
+            <Modal open = {open} setOpen={setOpen} />
         </div>
     );
 }
